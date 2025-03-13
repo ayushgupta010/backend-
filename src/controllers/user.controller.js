920c6b54806051e3b8810cf52235eca91e086614
ayushgupta010 <129612267+ayushgupta010@users.noreply.github.com>
2025-03-14 02:48:42 +0530

    adding login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../models/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateRefreshandAccessToken = async(userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = generateAccessToken()
+        const refreshToken = generateRefreshToken()
+
+        user.refreshToken = refreshToken 
+        await user.save({validateBeforeSafe: false})
+
+        return{accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generateing refrsh and acccess token")
+    }
+}
+
 const registerUser = asyncHandler( async (req,res)=>{
     //get user details
     //validation nnot empty
@@ -16,7 +31,7 @@ const registerUser = asyncHandler( async (req,res)=>{
     //return res
 
     const {fullName, email, username, password}=req.body
-    console.log("email", email)
+   // console.log("email", email)
     
     //validation
     if(
@@ -38,11 +53,14 @@ const registerUser = asyncHandler( async (req,res)=>{
 
     //accessing other things through middleware
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverimage[0]?.path
+    //const coverImageLocalPath = req.files?.coverimage[0]?.path
 
-    if(!coverImageLocalPath){
-        throw new ApiError(400, "cover image is required")
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverimage)
+    &&req.files.coverimage.length>0) {
+        coverImageLocalPath=req.files.coverimage[0].path
     }
+
     if(!avatarLocalPath){
         throw new ApiError(400, "avatar is required")
     }
@@ -54,10 +72,6 @@ const registerUser = asyncHandler( async (req,res)=>{
 
     if(!avatar){
         throw new ApiError(400, "avatar is required")
-    }
-
-    if(!coverimage){
-        throw new ApiError(400, "cover image is required")  
     }
 
     //creating user object in db and saving it to the database using mongoose model function.
@@ -83,4 +97,95 @@ const registerUser = asyncHandler( async (req,res)=>{
     )
 })
 
-export { registerUser }
+const loginUser = asyncHandler(async(req, res)=>{
+    //req.body-> data
+    //username or email
+    //find user
+    //password check
+    //access and refresh token
+    //send cookie
+
+    const{email, username, password}=req.body
+
+    if(!username||!email){
+        throw new ApiError(400,"username or email is requierd")
+    }
+
+    const user = await User.findOne({
+        $or : [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    //check password
+
+    const ifPasswordValid = await user.ifPasswordCorrect(password)
+
+    if(!password){
+        throw new ApiError(401, "Password is incorrect")
+    }
+
+    await generateRefreshandAccessToken(user._id )
+
+    const logedInUser = await user.findOne(user._id)
+    .select("-password -refreshToken")
+
+    //send token to the user
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+              user:logedInUser, accessToken, 
+            }
+        ),
+        "user loggedIn succesfully"
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearcookie("accessToken", options)
+    .clearcookie("refreshToken", options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "user loggedout"
+        )
+    )
+})
+export{
+     registerUser,
+     loginUser,
+     logoutUser
+}
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         message, 
         statusCode,
         errors={},
-        statck=""
+        stack=''
     ){
         super(message)
         this.statusCode=statusCode
